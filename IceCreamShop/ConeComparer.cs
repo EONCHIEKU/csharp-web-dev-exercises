e3cd870a63afd5ef45c49004739437021edb224b
Esther Onchieku <estheronchieku@gmail.com>
2023-03-16 16:09:54 -0500

    IceCreamShop

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCreamShop
+{
+    public class ConeComparer : IComparer<Cone>
+    {
+        public int Compare(Cone x, Cone y)
+        {
+            double costDifference = x.Cost - y.Cost;
+            if(costDifference > 0)
+            {
+                return 1;
+            }
+            else if(costDifference < 0)
+            {
+                return -1;
+            }
+            
+            
+                return 0;
+            
+            
+        }
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -6,8 +6,39 @@ List<Cone> availableCones = menu.Cones;
 
 // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
 //  field.
+//FlavorComparer superGood = new FlavorComparer();
 
+//foreach (Flavor flav in availableFlavors)
+//{
+//    Console.WriteLine(flav.Name);
+//}
+
+availableFlavors.Sort(new FlavorComparer());
+//availableFlavors.Sort(superGood);**This is the same as above line and with the above line we can do without declaring and initializing the Comparer Object named superGood.
+Console.WriteLine(">>>>>\n");
+foreach (Flavor flav in availableFlavors)
+{
+    Console.WriteLine(flav.Name);
+}
+Console.WriteLine("********************\n");
 // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
 //  field.
-
+//foreach(Cone cone in availableCones)
+//{
+//    Console.WriteLine($"{cone.Name}: {cone.Cost}");
+//}
+//Console.WriteLine(">>>>>>>>>>\n");
+availableCones.Sort(new ConeComparer());
+foreach(Cone cone in availableCones)
+{
+    Console.WriteLine($"{cone.Name}: {cone.Cost}");
+}
 // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+Console.WriteLine("********************\n");
+
+foreach (Flavor flav in availableFlavors)
+{
+    Console.WriteLine($"{flav.Name} Allergens: {flav.Allergens.Count}");
+}
+Console.WriteLine("********************\n");
+
